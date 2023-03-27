39e01df56e57731e60dc701f3f7af1b45aaede6c
Juan David <juandcifuentes16@gmail.com>
2023-03-27 02:19:24 -0500

    v0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegistroProjComponent } from './registro-proj/registro-proj.component';
-import { VisualItemComponent } from './visual-item/visual-item.component';
-import { RegistroItemComponent } from './registro-item/registro-item.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistroProjComponent,
-    VisualItemComponent,
-    RegistroItemComponent
+    
   ],
   imports: [
     BrowserModule,
